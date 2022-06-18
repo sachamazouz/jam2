8f66f152a8f2812eba9fbbbafe66e7913be1ca1a
Sacha MAZOUZ <sacha.mazouz@epitech.eu>
2022-06-18 19:57:20 +0200

    algorithm which fixing good value for x, I now have to be a lit more precise on the y values

--- src_file/check_family.c
+++ src_file/check_family.c
@@ -12,20 +12,17 @@ void check_family(display_struct_t *sprite_struct, enemy_pos_t *array)
 {
     int tour_de_boucle = 0;
     int cursor = 0;
-    int haut_gauchex = sprite_struct->s_walpa.posi.x - 50;
-    int haut_gauchey = sprite_struct->s_walpa.posi.y - 50;
-    int bas_droitx = sprite_struct->s_walpa.posi.x + 50;
-    int bas_droity = sprite_struct->s_walpa.posi.y + 50;
-
+    int x_main = 860.0 - 150;
+    int y_main = 400.0;
+    int x_main_max = x_main + 150;
+    int y_main_max = y_main + 130;
 
     printf("tick 1 est de coordonnée: "
     "\nx = %f\n y = %f\n",sprite_struct->a.s_tick1.posi.x,sprite_struct->a.s_tick1.posi.y);
 
-    if ((sprite_struct->a.s_tick1.posi.x) > sprite_struct->a.s_main.posi.x &&
-    (sprite_struct->a.s_tick1.posi.x + 200) < sprite_struct->a.s_main.posi.x &&
-    (sprite_struct->a.s_tick1.posi.y) > sprite_struct->a.s_main.posi.y &&
-    (sprite_struct->a.s_tick1.posi.y + 130) > sprite_struct->a.s_main.posi.y)
-    bas_droity++;
+    if (sprite_struct->a.s_tick1.posi.x > x_main && sprite_struct->a.s_tick1.posi.x < x_main_max
+    && sprite_struct->a.s_tick1.posi.y > y_main && sprite_struct->a.s_tick1.posi.y < y_main_max)
+        printf("into \n");
 
     return;
 }
